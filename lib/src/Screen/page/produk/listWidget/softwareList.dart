5ce7ca3b912b15f4345bcef6523fa83c3784c88e
root <auroralinkid@gmail.com>
2019-07-26 22:32:27 +0700

    tapbar produk detail

--- lib/src/Screen/page/produk/listWidget/softwareList.dart
+++ lib/src/Screen/page/produk/listWidget/softwareList.dart
@@ -1,6 +1,6 @@
 import 'package:auroralink/src/Bloc/Produk/ProdukBloc.dart';
 import 'package:auroralink/src/Models/Produk/ProdukModels.dart';
-import 'package:auroralink/src/Screen/page/produk/detailWidget/detailProduk.dart';
+import 'package:auroralink/src/Screen/page/produk/detailWidget/detailDesktopApp.dart';
 import 'package:auroralink/src/Screen/property/IconPallete.dart';
 import 'package:flutter/material.dart';
 import 'package:shimmer/shimmer.dart';
@@ -82,13 +82,106 @@ class _SoftwareState extends State<Software> {
                   baseColor: IconPallete.menuCar,
                   highlightColor: Colors.yellow,
                   child: Text(
-                    'Loading...',
+                    'Mohon Tunggu ...',
                     textAlign: TextAlign.center,
                   ),
                 ),
               );
             }),
-        // )
+            // StreamBuilder(
+            // stream: produkbloc.semuaproduk,
+            // builder: (context, AsyncSnapshot<DaftarProduk> snapshot) {
+            //   if (snapshot.hasData) {
+            //     return Container(
+            //         child: Column(
+            //       children: <Widget>[
+            //         Container(
+            //             height: 200.0,
+            //             decoration: new BoxDecoration(boxShadow: [
+            //               new BoxShadow(
+            //                 color: Color(0xffeeeeee),
+            //                 blurRadius: 1.0,
+            //                 offset: new Offset(1.0, 1.0),
+            //               ),
+            //             ]),
+            //             child: Card(
+            //               child: InkWell(
+            //                 child: Column(
+            //                   crossAxisAlignment: CrossAxisAlignment.start,
+            //                   children: <Widget>[
+            //                     Padding(
+            //                       padding: const EdgeInsets.only(
+            //                           left: 10.0, top: 10.0),
+            //                       child: Text(
+            //                         "WebApp",
+            //                         style: TextStyle(
+            //                             color: Color(0xff616161),
+            //                             fontSize: 16.0),
+            //                       ),
+            //                     ),
+            //                     Expanded(
+            //                       child: webapplist(snapshot),
+            //                     )
+            //                   ],
+            //                 ),
+            //               ),
+            //             ))
+            //       ],
+            //     ));
+            //   } else if (snapshot.hasError) {
+            //     return Text(
+            //       snapshot.error.toString(),
+            //     );
+            //   }
+            //   return Center();
+            // }),
+            // StreamBuilder(
+            // stream: produkbloc.semuaproduk,
+            // builder: (context, AsyncSnapshot<DaftarProduk> snapshot) {
+            //   if (snapshot.hasData) {
+            //     return Container(
+            //         child: Column(
+            //       children: <Widget>[
+            //         Container(
+            //             height: 200.0,
+            //             decoration: new BoxDecoration(boxShadow: [
+            //               new BoxShadow(
+            //                 color: Color(0xffeeeeee),
+            //                 blurRadius: 1.0,
+            //                 offset: new Offset(1.0, 1.0),
+            //               ),
+            //             ]),
+            //             child: Card(
+            //               child: InkWell(
+            //                 child: Column(
+            //                   crossAxisAlignment: CrossAxisAlignment.start,
+            //                   children: <Widget>[
+            //                     Padding(
+            //                       padding: const EdgeInsets.only(
+            //                           left: 10.0, top: 10.0),
+            //                       child: Text(
+            //                         "MobileApp",
+            //                         style: TextStyle(
+            //                             color: Color(0xff616161),
+            //                             fontSize: 16.0),
+            //                       ),
+            //                     ),
+            //                     Expanded(
+            //                       child: mobapplist(snapshot),
+            //                     )
+            //                   ],
+            //                 ),
+            //               ),
+            //             ))
+            //       ],
+            //     ));
+            //   } else if (snapshot.hasError) {
+            //     return Text(
+            //       snapshot.error.toString(),
+            //     );
+            //   }
+            //   return Center();
+            // }),
       ],
     )));
   }
@@ -117,14 +210,26 @@ Widget aplikasidesktoplist(AsyncSnapshot<DaftarProduk> snapshot) {
                                     buildcontext,
                                     MaterialPageRoute(
                                       builder: (context) => DetailPage(
-                                        nama: snapshot.data.produk[index].nama, gambar: snapshot.data.produk[index].img1,
+                                        nama: snapshot.data.produk[index].nama,
+                                        gambar:
+                                            snapshot.data.produk[index].img1,
+                                        deskripsi: snapshot
+                                            .data.produk[index].deskripsi,
+                                        f1: snapshot.data.produk[index].f1,
+                                        f2: snapshot.data.produk[index].f2,
+                                        f3: snapshot.data.produk[index].f3,
+                                        f4: snapshot.data.produk[index].f4,
+                                        f5: snapshot.data.produk[index].f5,
+                                        f6: snapshot.data.produk[index].f6,
+                                        v1: snapshot.data.produk[index].v1,
                                       ),
                                     ),
                                   ),
                               child: Column(
                                 crossAxisAlignment: CrossAxisAlignment.center,
                                 children: <Widget>[
-                                  Image.network(snapshot.data.produk[index].img1,
+                                  Image.network(
+                                    snapshot.data.produk[index].img1,
                                     height: 100,
                                     width: 200,
                                   ),
@@ -147,120 +252,130 @@ Widget aplikasidesktoplist(AsyncSnapshot<DaftarProduk> snapshot) {
       });
 }
 
-Widget webapplist(AsyncSnapshot<DaftarProduk> snapshot) {
-  return Container(
-      height: 200.0,
-      decoration: new BoxDecoration(boxShadow: [
-        new BoxShadow(
-          color: Color(0xffeeeeee),
-          blurRadius: 1.0,
-          offset: new Offset(1.0, 1.0),
-        ),
-      ]),
-      child: Card(
-          child: InkWell(
-              child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.only(left: 10.0, top: 10.0),
-              child: Text(
-                "Kategori WebApp",
-                style: TextStyle(color: Color(0xff616161), fontSize: 16.0),
-              ),
-            ),
-            Expanded(
-                child: ListView.builder(
-                    scrollDirection: Axis.horizontal,
-                    itemCount: snapshot.data.produk.length,
-                    itemBuilder: (buildcontext, int index) {
-                      return Card(
-                        child: Column(
-                          children: <Widget>[
-                            Padding(
-                              padding: const EdgeInsets.all(10.0),
-                              child: Container(
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.center,
-                                  children: <Widget>[
-                                    Image.network(
-                                      "https://auroralink.id/uploads/1/2019-06/smpn_54.png",
-                                      height: 100,
-                                      width: 200,
-                                    ),
-                                    Padding(
-                                      padding: EdgeInsets.all(2.0),
-                                    ),
-                                    Text(snapshot.data.produk[index].nama),
-                                    Padding(
-                                      padding: EdgeInsets.all(2.0),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      );
-                    }))
-          ]))));
-}
+// Widget webapplist(AsyncSnapshot<DaftarProduk> snapshot) {
+//   return ListView.builder(
+//       scrollDirection: Axis.horizontal,
+//       itemCount: snapshot.data.produk.length,
+//       itemBuilder: (buildcontext, int index) {
+//         return Card(
+//           child: Column(
+//             children: <Widget>[
+//               Padding(
+//                   padding: const EdgeInsets.all(10.0),
+//                   child: Hero(
+//                     tag: snapshot.data.produk[index].nama,
+//                     transitionOnUserGestures: true,
+//                     child: Container(
+//                       child: Column(
+//                         crossAxisAlignment: CrossAxisAlignment.center,
+//                         children: <Widget>[
+//                           InkWell(
+//                               // onTap: () => Navigator.push(
+//                               //       buildcontext,
+//                               //       MaterialPageRoute(
+//                               //         builder: (context) => DetailPage(
+//                               //           nama: snapshot.data.produk[index].nama,
+//                               //           gambar:
+//                               //               snapshot.data.produk[index].img1,
+//                               //           deskripsi: snapshot
+//                               //               .data.produk[index].deskripsi,
+//                               //           f1: snapshot.data.produk[index].f1,
+//                               //           f2: snapshot.data.produk[index].f2,
+//                               //           f3: snapshot.data.produk[index].f3,
+//                               //           f4: snapshot.data.produk[index].f4,
+//                               //           f5: snapshot.data.produk[index].f5,
+//                               //           f6: snapshot.data.produk[index].f6,
+//                               //           v1: snapshot.data.produk[index].v1,
+//                               //         ),
+//                               //       ),
+//                               //     ),
+//                               child: Column(
+//                                 crossAxisAlignment: CrossAxisAlignment.center,
+//                                 children: <Widget>[
+//                                   Image.network(
+//                                     snapshot.data.produk[index].img1,
+//                                     height: 100,
+//                                     width: 200,
+//                                   ),
+//                                   Padding(
+//                                     padding: EdgeInsets.all(2.0),
+//                                   ),
+//                                   Text(snapshot.data.produk[index].nama),
+//                                   Padding(
+//                                     padding: EdgeInsets.all(2.0),
+//                                   ),
+//                                 ],
+//                               ))
+//                         ],
+//                       ),
+//                     ),
+//                   )),
+//             ],
+//           ),
+//         );
+//       });
+// }
 
-Widget mobapplist(AsyncSnapshot<DaftarProduk> snapshot) {
-  return Container(
-      height: 200.0,
-      decoration: new BoxDecoration(boxShadow: [
-        new BoxShadow(
-          color: Color(0xffeeeeee),
-          blurRadius: 1.0,
-          offset: new Offset(1.0, 1.0),
-        ),
-      ]),
-      child: Card(
-          child: InkWell(
-              child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.only(left: 10.0, top: 10.0),
-              child: Text(
-                "Kategori MobileApp",
-                style: TextStyle(color: Color(0xff616161), fontSize: 16.0),
-              ),
-            ),
-            Expanded(
-                child: ListView.builder(
-                    scrollDirection: Axis.horizontal,
-                    itemCount: snapshot.data.produk.length,
-                    itemBuilder: (buildcontext, int index) {
-                      return Card(
-                        child: Column(
-                          children: <Widget>[
-                            Padding(
-                              padding: const EdgeInsets.all(10.0),
-                              child: Container(
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.center,
-                                  children: <Widget>[
-                                    Image.network(
-                                      "https://auroralink.id/uploads/1/2019-06/smpn_54.png",
-                                      height: 100,
-                                      width: 200,
-                                    ),
-                                    Padding(
-                                      padding: EdgeInsets.all(2.0),
-                                    ),
-                                    Text(snapshot.data.produk[index].nama),
-                                    Padding(
-                                      padding: EdgeInsets.all(2.0),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      );
-                    }))
-          ]))));
-}
+// Widget mobapplist(AsyncSnapshot<DaftarProduk> snapshot) {
+//   return ListView.builder(
+//       scrollDirection: Axis.horizontal,
+//       itemCount: snapshot.data.produk.length,
+//       itemBuilder: (buildcontext, int index) {
+//         return Card(
+//           child: Column(
+//             children: <Widget>[
+//               Padding(
+//                   padding: const EdgeInsets.all(10.0),
+//                   child: Hero(
+//                     tag: snapshot.data.produk[index].nama,
+//                     transitionOnUserGestures: true,
+//                     child: Container(
+//                       child: Column(
+//                         crossAxisAlignment: CrossAxisAlignment.center,
+//                         children: <Widget>[
+//                           InkWell(
+//                               // onTap: () => Navigator.push(
+//                               //       buildcontext,
+//                               //       MaterialPageRoute(
+//                               //         builder: (context) => DetailPage(
+//                               //           nama: snapshot.data.produk[index].nama,
+//                               //           gambar:
+//                               //               snapshot.data.produk[index].img1,
+//                               //           deskripsi: snapshot
+//                               //               .data.produk[index].deskripsi,
+//                               //           f1: snapshot.data.produk[index].f1,
+//                               //           f2: snapshot.data.produk[index].f2,
+//                               //           f3: snapshot.data.produk[index].f3,
+//                               //           f4: snapshot.data.produk[index].f4,
+//                               //           f5: snapshot.data.produk[index].f5,
+//                               //           f6: snapshot.data.produk[index].f6,
+//                               //           v1: snapshot.data.produk[index].v1,
+//                               //         ),
+//                               //       ),
+//                               //     ),
+//                               child: Column(
+//                                 crossAxisAlignment: CrossAxisAlignment.center,
+//                                 children: <Widget>[
+//                                   Image.network(
+//                                     snapshot.data.produk[index].img1,
+//                                     height: 100,
+//                                     width: 200,
+//                                   ),
+//                                   Padding(
+//                                     padding: EdgeInsets.all(2.0),
+//                                   ),
+//                                   Text(snapshot.data.produk[index].nama),
+//                                   Padding(
+//                                     padding: EdgeInsets.all(2.0),
+//                                   ),
+//                                 ],
+//                               ))
+//                         ],
+//                       ),
+//                     ),
+//                   )),
+//             ],
+//           ),
+//         );
+//       });
+// }
